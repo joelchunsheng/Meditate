77a804e7bb956c152b8398d16220b8df62cb1e95
Joel Yeo <joel.chunsheng@gmail.com>
2020-04-28 19:08:36 +0800

    Added tab nav bar
    created tab nav bar

--- app/src/main/java/com/android/meditate/MainActivity.java
+++ app/src/main/java/com/android/meditate/MainActivity.java
@@ -1,14 +1,66 @@
 package com.android.meditate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private MoodFragment moodFragment;
+    private UserFragment userFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        moodFragment = new MoodFragment();
+        userFragment = new UserFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.navBarHome:
+                        setFragment(homeFragment);
+                        return true;
+
+                    case R.id.navBarMood:
+                        setFragment(moodFragment);
+                        return true;
+
+                    case R.id.navBarUser:
+                        setFragment(userFragment);
+                        return true;
+
+                    default:
+                        return false;
+
+                }
+            }
+        });
+    }
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
