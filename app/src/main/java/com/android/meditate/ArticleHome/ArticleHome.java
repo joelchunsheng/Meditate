ce2be2c52d18b7e25110cd264b11d6923560a29e
Joel Yeo <joel.chunsheng@gmail.com>
2020-07-06 21:35:05 +0800

    add font

--- app/src/main/java/com/android/meditate/ArticleHome/ArticleHome.java
+++ app/src/main/java/com/android/meditate/ArticleHome/ArticleHome.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.android.meditate.MainActivity;
@@ -20,14 +21,14 @@ public class ArticleHome extends AppCompatActivity {
     private RecyclerView smallArticle;
     private ArrayList<ArticleModel> listBigArticles;
     private ArrayList<ArticleModel> listSmallArticles;
-    ImageView backBtn;
+    ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_home);
 
-        backBtn = findViewById(R.id.maBackImg);
+        backBtn = findViewById(R.id.maBackBtn);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
