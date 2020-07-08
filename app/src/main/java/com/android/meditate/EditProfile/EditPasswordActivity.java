f01d86754ddd84f76c95b5aec88a61b0140b56e8
Joel Yeo <joel.chunsheng@gmail.com>
2020-07-09 03:30:16 +0800

    complete update username

---
+++ app/src/main/java/com/android/meditate/EditProfile/EditPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.android.meditate.EditProfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.meditate.R;
+
+public class EditPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_password);
+    }
+}
